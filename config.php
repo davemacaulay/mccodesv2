a3ef6dd7975e2e117c8b3c13b9733fcfebab6b30
Orsokuma <magictallguy@hotmail.com>
2024-06-20 02:42:48 +0100

    Ignore generated config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+$_CONFIG = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '',
+    'database' => 'somedb',
+    'code' => '123bnuc9312cn786531798cn1089342j893145',
+];
