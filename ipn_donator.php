b9a111fcc69c30afd63f711a37cff767b7fe6f61
Orsokuma <magictallguy@hotmail.com>
2024-06-20 03:57:26 +0100

    Invert `if` statements to negate empty bodies

--- ipn_donator.php
+++ ipn_donator.php
@@ -47,32 +47,23 @@ $txn_id = $_POST['txn_id'];
 $receiver_email = $_POST['receiver_email'];
 $payer_email = $_POST['payer_email'];
 
-if (!$fp)
-{
-    // HTTP ERROR
-}
-else
-{
+if ($fp) {
     fputs($fp, $header . $req);
-    while (!feof($fp))
-    {
+    while (!feof($fp)) {
         $res = fgets($fp, 1024);
-        if (strcmp($res, 'VERIFIED') == 0)
-        {
+        if (strcmp($res, 'VERIFIED') == 0) {
             $txn_db = $db->escape(stripslashes($txn_id));
             // check the payment_status is Completed
-            if ($payment_status != 'Completed')
-            {
+            if ($payment_status != 'Completed') {
                 fclose($fp);
                 die('');
             }
             $dp_check =
-                    $db->query(
-                            "SELECT COUNT(`dpID`)
+                $db->query(
+                    "SELECT COUNT(`dpID`)
                              FROM `dps_accepted`
                              WHERE `dpTXN` = '{$txn_db}'");
-            if ($db->fetch_single($dp_check) > 0)
-            {
+            if ($db->fetch_single($dp_check) > 0) {
                 $db->free_result($dp_check);
                 fclose($fp);
                 die('');
@@ -80,61 +71,52 @@ else
             $db->free_result($dp_check);
             // check that txn_id has not been previously processed
             // check that receiver_email is your Primary PayPal email
-            if ($receiver_email != $set['paypal'])
-            {
+            if ($receiver_email != $set['paypal']) {
                 fclose($fp);
                 die('');
             }
             // check that payment_amount/payment_currency are correct
-            if ($payment_currency != 'USD')
-            {
+            if ($payment_currency != 'USD') {
                 fclose($fp);
                 die('');
             }
             // parse for pack
             $packr = explode('|', $item_name);
             if (str_replace('www.', '', $packr[0])
-                    != str_replace('www.', '', $_SERVER['HTTP_HOST']))
-            {
+                != str_replace('www.', '', $_SERVER['HTTP_HOST'])) {
                 fclose($fp);
                 die('');
             }
-            if ($packr[1] != 'DP')
-            {
+            if ($packr[1] != 'DP') {
                 fclose($fp);
                 die('');
             }
             $pack = $packr[2];
             if ($pack != 1 and $pack != 2 and $pack != 3 and $pack != 4
-                    and $pack != 5)
-            {
+                and $pack != 5) {
                 fclose($fp);
                 die('');
             }
             if (($pack == 1 || $pack == 2 || $pack == 3)
-                    && $payment_amount != '3.00')
-            {
+                && $payment_amount != '3.00') {
                 fclose($fp);
                 die('');
             }
-            if ($pack == 4 && $payment_amount != '5.00')
-            {
+            if ($pack == 4 && $payment_amount != '5.00') {
                 fclose($fp);
                 die('');
             }
-            if ($pack == 5 && $payment_amount != '10.00')
-            {
+            if ($pack == 5 && $payment_amount != '10.00') {
                 fclose($fp);
                 die('');
             }
             // grab IDs
-            $buyer = abs((int) $packr[3]);
-            $for = $buyer;
+            $buyer = abs((int)$packr[3]);
+            $for   = $buyer;
             // all seems to be in order, credit it.
-            if ($pack == 1)
-            {
+            if ($pack == 1) {
                 $db->query(
-                        "UPDATE `users` AS `u`
+                    "UPDATE `users` AS `u`
                          LEFT JOIN `userstats` AS `us`
                          ON `u`.`userid` = `us`.`userid`
                          SET `u`.`money` = `u`.`money` + 5000,
@@ -144,21 +126,17 @@ else
                          WHERE `u`.`userid` = {$for}");
                 $d = 30;
                 $t = 'standard';
-            }
-            else if ($pack == 2)
-            {
+            } elseif ($pack == 2) {
                 $db->query(
-                        "UPDATE `users` AS `u`
+                    "UPDATE `users` AS `u`
                          SET `u`.`crystals` = `u`.`crystals` + 100,
                          `u`.`donatordays` = `u`.`donatordays` + 30
                          WHERE `u`.`userid` = {$for}");
                 $d = 30;
                 $t = 'crystals';
-            }
-            else if ($pack == 3)
-            {
+            } elseif ($pack == 3) {
                 $db->query(
-                        "UPDATE `users` AS `u`
+                    "UPDATE `users` AS `u`
                          LEFT JOIN `userstats` AS `us`
                          ON `u`.`userid` = `us`.`userid`
                          SET `us`.`IQ` = `us`.`IQ` + 50,
@@ -166,11 +144,9 @@ else
                          WHERE `u`.`userid` = {$for}");
                 $d = 30;
                 $t = 'iq';
-            }
-            else if ($pack == 4)
-            {
+            } elseif ($pack == 4) {
                 $db->query(
-                        "UPDATE `users` AS `u`
+                    "UPDATE `users` AS `u`
                          LEFT JOIN `userstats` AS `us`
                          ON `u`.`userid` = `us`.`userid`
                          SET `u`.`money` = `u`.`money` + 15000,
@@ -180,11 +156,9 @@ else
                          WHERE `u`.`userid` = {$for}");
                 $d = 55;
                 $t = 'fivedollars';
-            }
-            else if ($pack == 5)
-            {
+            } elseif ($pack == 5) {
                 $db->query(
-                        "UPDATE `users` AS `u`
+                    "UPDATE `users` AS `u`
                          LEFT JOIN `userstats` AS `us`
                          ON `u`.`userid` = `us`.`userid`
                          SET `u`.`money` = `u`.`money` + 35000,
@@ -199,12 +173,10 @@ else
             event_add($for,
                 "Your \${$payment_amount} Pack {$pack} Donator Pack has been successfully credited to you.");
             $db->query(
-                    "INSERT INTO `dps_accepted`
+                "INSERT INTO `dps_accepted`
                      VALUES(NULL, {$buyer}, {$for}, '$t', " . time()
-                            . ", '$txn_db')");
-        }
-        else if (strcmp($res, 'INVALID') == 0)
-        {
+                . ", '$txn_db')");
+        } elseif (strcmp($res, 'INVALID') == 0) {
         }
     }
 
--- ipn_wp.php
+++ ipn_wp.php
@@ -46,44 +46,34 @@ $txn_id = $_POST['txn_id'];
 $receiver_email = $_POST['receiver_email'];
 $payer_email = $_POST['payer_email'];
 
-if (!$fp)
-{
-    // HTTP ERROR
-}
-else
-{
+if ($fp) {
     fputs($fp, $header . $req);
-    while (!feof($fp))
-    {
+    while (!feof($fp)) {
         $res = fgets($fp, 1024);
-        if (strcmp($res, 'VERIFIED') == 0)
-        {
+        if (strcmp($res, 'VERIFIED') == 0) {
             $txn_db = $db->escape(stripslashes($txn_id));
             // check the payment_status is Completed
-            if ($payment_status != 'Completed')
-            {
+            if ($payment_status != 'Completed') {
                 fclose($fp);
                 die('');
             }
             $dp_check =
-                    $db->query(
-                            "SELECT COUNT(`dpID`)
+                $db->query(
+                    "SELECT COUNT(`dpID`)
                              FROM `dps_accepted`
                              WHERE `dpTXN` = '{$txn_db}'");
-            if ($db->fetch_single($dp_check) > 0)
-            {
+            if ($db->fetch_single($dp_check) > 0) {
                 $db->free_result($dp_check);
                 fclose($fp);
                 die('');
             }
             $db->free_result($dp_check);
             $wp_check =
-                    $db->query(
-                            "SELECT COUNT(`dpID`)
+                $db->query(
+                    "SELECT COUNT(`dpID`)
                              FROM `willps_accepted`
                              WHERE `dpTXN` = '{$txn_db}'");
-            if ($db->fetch_single($wp_check) > 0)
-            {
+            if ($db->fetch_single($wp_check) > 0) {
                 $db->free_result($wp_check);
                 fclose($fp);
                 die('');
@@ -91,57 +81,47 @@ else
             $db->free_result($wp_check);
             // check that txn_id has not been previously processed
             // check that receiver_email is your Primary PayPal email
-            if ($receiver_email != $set['paypal'])
-            {
+            if ($receiver_email != $set['paypal']) {
                 fclose($fp);
                 die('');
             }
             // check that payment_amount/payment_currency are correct
-            if ($payment_currency != 'USD')
-            {
+            if ($payment_currency != 'USD') {
                 fclose($fp);
                 die('');
             }
             // parse for pack
             $packr = explode('|', $item_name);
             if (str_replace('www.', '', $packr[0])
-                    != str_replace('www.', '', $_SERVER['HTTP_HOST']))
-            {
+                != str_replace('www.', '', $_SERVER['HTTP_HOST'])) {
                 fclose($fp);
                 die('');
             }
-            if ($packr[1] != 'WP')
-            {
+            if ($packr[1] != 'WP') {
                 fclose($fp);
                 die('');
             }
             $pack = $packr[2];
-            if ($pack != 1 and $pack != 5)
-            {
+            if ($pack != 1 and $pack != 5) {
                 fclose($fp);
                 die('');
             }
-            if (($pack == 1) && $payment_amount != '1.00')
-            {
+            if (($pack == 1) && $payment_amount != '1.00') {
                 fclose($fp);
                 die('');
             }
-            if ($pack == 5 && $payment_amount != '4.50')
-            {
+            if ($pack == 5 && $payment_amount != '4.50') {
                 fclose($fp);
                 die('');
             }
             // grab IDs
-            $buyer = abs((int) $packr[3]);
-            $for = $buyer;
+            $buyer = abs((int)$packr[3]);
+            $for   = $buyer;
             // all seems to be in order, credit it.
-            if ($pack == 1)
-            {
+            if ($pack == 1) {
                 item_add($for, $set['willp_item'], 1);
 
-            }
-            else if ($pack == 5)
-            {
+            } elseif ($pack == 5) {
                 item_add($for, $set['willp_item'], 5);
 
             }
@@ -150,12 +130,10 @@ else
             event_add($for,
                 "Your \${$payment_amount} worth of Will Potions ($pack) has been successfully credited.");
             $db->query(
-                    "INSERT INTO `willps_accepted`
+                "INSERT INTO `willps_accepted`
                      VALUES(NULL, {$buyer}, {$for}, '$pack', " . time()
-                            . ", '$txn_db')");
-        }
-        else if (strcmp($res, 'INVALID') == 0)
-        {
+                . ", '$txn_db')");
+        } elseif (strcmp($res, 'INVALID') == 0) {
         }
     }
     fclose($fp);
