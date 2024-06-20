f56627dcba68575b00c6e8a3fd363f731122c83e
Orsokuma <magictallguy@hotmail.com>
2024-06-20 04:01:06 +0100

    Remove deprecated `<center>` tag

--- installer_head.php
+++ installer_head.php
@@ -34,18 +34,17 @@ ob_start();
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
-    <center>
-        <table width="970" border="0" cellpadding="0" cellspacing="0"
-            class="table2">
-            <tr>
-                <td class="lgrad"></td>
-                <td class="center"><img src="title.jpg"
-                    alt="Mccodes Version 2" /><br /> <!-- Begin Main Content -->
-                    <h2>Installer</h2> <span
-                    style='color: red; font-weight: bold;'> If
-                        you bought this from anywhere else but <a
-                                href='https://www.mccodes.com' title='MCCodes'>
-                            MCCodes.com </a> please click <a
-                        href='https://www.mccodes.com/contact.php'
-                        title='Contact us'> here </a>.</span> <br /> Thank you
-                    for choosing MCCodes.
+    <table width="970" border="0" cellpadding="0" cellspacing="0"
+        class="table2">
+        <tr>
+            <td class="lgrad"></td>
+            <td class="center"><img src="title.jpg"
+                alt="Mccodes Version 2" /><br /> <!-- Begin Main Content -->
+                <h2>Installer</h2> <span
+                style='color: red; font-weight: bold;'> If
+                    you bought this from anywhere else but <a
+                            href='https://www.mccodes.com' title='MCCodes'>
+                        MCCodes.com </a> please click <a
+                    href='https://www.mccodes.com/contact.php'
+                    title='Contact us'> here </a>.</span> <br /> Thank you
+                for choosing MCCodes.
