37b8bad3bf3f6c2856bf564557578dc2e22502fb
Orsokuma <magictallguy@hotmail.com>
2024-06-20 17:55:32 +0100

    stafflist to use new roles system

--- stafflist.php
+++ stafflist.php
@@ -25,24 +25,25 @@ declare(strict_types=1);
 global $db, $h;
 require_once('globals.php');
 $staff = [];
-$q =
-        $db->query(
-            'SELECT `userid`, `laston`, `username`, `level`, `money`,
- 				 `user_level`
- 				 FROM `users`
- 				 WHERE `user_level` IN(2, 3, 5)
- 				 ORDER BY `userid` ASC');
-while ($r = $db->fetch_row($q))
-{
+$q     = $db->query(
+    'SELECT u.userid, u.laston, u.username, u.level, u.money, u.staff_roles, GROUP_CONCAT(sr.name ORDER BY sr.id) AS roles
+    FROM users AS u
+    INNER JOIN staff_roles AS sr ON FIND_IN_SET(sr.id, u.staff_roles)
+    WHERE u.staff_roles != \'\'
+    GROUP BY u.userid
+    ORDER BY u.userid'
+);
+while ($r = $db->fetch_row($q)) {
     $staff[$r['userid']] = $r;
 }
 $db->free_result($q);
 echo '
-<b>Admins</b>
+<b>Staff</b>
 <br />
-<table width="75%" cellspacing="1" cellpadding="1" class="table">
+<table style="width: 75%; padding: 1px;" class="table">
 		<tr>
 			<th>User</th>
+			<th>Roles</th>
 			<th>Level</th>
 			<th>Money</th>
 			<th>Last Seen</th>
@@ -50,91 +51,20 @@ echo '
 		</tr>
    ';
 
-foreach ($staff as $r)
-{
-    if ($r['user_level'] == 2)
-    {
-        $on =
-                ($r['laston'] >= ($_SERVER['REQUEST_TIME'] - 900))
-                        ? '<span style="color: green;">Online</span>'
-                        : '<span style="color: green;">Offline</span>';
-        echo '
-		<tr>
-			<td><a href="viewuser.php?u=' . $r['userid'] . '">'
-                . $r['username'] . '</a> [' . $r['userid'] . ']</td>
-			<td>' . $r['level'] . '</td>
-			<td>' . money_formatter((int)$r['money']) . '</td>
-			<td>' . date('F j, Y, g:i:s a', (int)$r['laston']) . '</td>
-			<td>' . $on . '</td>
-		</tr>
-   		';
-    }
-}
-echo '</table>
-
-<b>Secretaries</b>
-<br />
-<table width="75%" cellspacing="1" cellpadding="1" class="table">
-		<tr>
-			<th>User</th>
-			<th>Level</th>
-			<th>Money</th>
-			<th>Last Seen</th>
-			<th>Status</th>
-		</tr>
-   ';
-foreach ($staff as $r)
-{
-    if ($r['user_level'] == 3)
-    {
-        $on =
-                ($r['laston'] >= ($_SERVER['REQUEST_TIME'] - 900))
-                        ? '<span style="color: green;">Online</span>'
-                        : '<span style="color: green;">Offline</span>';
-        echo '
-		<tr>
-			<td><a href="viewuser.php?u=' . $r['userid'] . '">'
-                . $r['username'] . '</a> [' . $r['userid'] . ']</td>
-			<td>' . $r['level'] . '</td>
-			<td>' . money_formatter((int)$r['money']) . '</td>
-			<td>' . date('F j, Y, g:i:s a', (int)$r['laston']) . '</td>
-			<td>' . $on . '</td>
-		</tr>
-   		';
-    }
-}
-echo '</table>
-
-<b>Assistants</b>
-<br />
-<table width="75%" cellspacing="1" cellpadding="1" class="table">
-		<tr>
-			<th>User</th>
-			<th>Level</th>
-			<th>Money</th>
-			<th>Last Seen</th>
-			<th>Status</th>
-		</tr>
-   ';
-foreach ($staff as $r)
-{
-    if ($r['user_level'] == 5)
-    {
-        $on =
-                ($r['laston'] >= ($_SERVER['REQUEST_TIME'] - 900))
-                        ? '<span style="color: green;">Online</span>'
-                        : '<span style="color: green;">Offline</span>';
-        echo '
-		<tr>
-			<td><a href="viewuser.php?u=' . $r['userid'] . '">'
-                . $r['username'] . '</a> [' . $r['userid'] . ']</td>
-			<td>' . $r['level'] . '</td>
-			<td>' . money_formatter((int)$r['money']) . '</td>
-			<td>' . date('F j, Y, g:i:s a', (int)$r['laston']) . '</td>
-			<td>' . $on . '</td>
-		</tr>
-   		';
-    }
+foreach ($staff as $r) {
+    $on = ($r['laston'] >= ($_SERVER['REQUEST_TIME'] - 900))
+        ? '<span style="color: green;">Online</span>'
+        : '<span style="color: green;">Offline</span>';
+    echo '
+        <tr>
+            <td><a href="viewuser.php?u=' . $r['userid'] . '">' . $r['username'] . '</a> [' . $r['userid'] . ']</td>
+            <td>' . str_replace(',', ', ', $r['roles']) . '</td>
+            <td>' . $r['level'] . '</td>
+            <td>' . money_formatter((int)$r['money']) . '</td>
+            <td>' . date('F j, Y, g:i:s a', (int)$r['laston']) . '</td>
+            <td>' . $on . '</td>
+        </tr>
+    ';
 }
 echo '</table>';
 $h->endpage();
