f61a4e42b04133f01c870f79dddc31b8105ea644
Orsokuma <magictallguy@hotmail.com>
2024-06-20 19:14:30 +0100

    Update stafflist to match new staff roles

--- stafflist.php
+++ stafflist.php
@@ -26,10 +26,11 @@ global $db, $h;
 require_once('globals.php');
 $staff = [];
 $q     = $db->query(
-    'SELECT u.userid, u.laston, u.username, u.level, u.money, u.staff_roles, GROUP_CONCAT(sr.name ORDER BY sr.id) AS roles
+    'SELECT u.userid, u.laston, u.username, u.level, u.money, GROUP_CONCAT(sr.name ORDER BY sr.id) AS roles
     FROM users AS u
-    INNER JOIN staff_roles AS sr ON FIND_IN_SET(sr.id, u.staff_roles)
-    WHERE u.staff_roles != \'\'
+    INNER JOIN users_roles AS ur ON ur.userid = u.userid
+    INNER JOIN staff_roles AS sr ON sr.id = ur.staff_role
+    WHERE ur.staff_role > 0
     GROUP BY u.userid
     ORDER BY u.userid'
 );
