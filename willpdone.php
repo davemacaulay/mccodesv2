46b18ccad7f26d0e1c3cc6c23a8831a1ef92f234
Dave Macaulay <daavem@googlemail.com>
2024-06-23 08:24:44 -0500

    Update license in all files

--- willpdone.php
+++ willpdone.php
@@ -1,24 +1,10 @@
 <?php
 declare(strict_types=1);
 /**
- * MCCodes Version 2.0.5b
- * Copyright (C) 2005-2012 Dabomstew
- * All rights reserved.
- *
- * Redistribution of this code in any form is prohibited, except in
- * the specific cases set out in the MCCodes Customer License.
- *
- * This code license may be used to run one (1) game.
- * A game is defined as the set of users and other game database data,
- * so you are permitted to create alternative clients for your game.
- *
- * If you did not obtain this code from MCCodes.com, you are in all likelihood
- * using it illegally. Please contact MCCodes to discuss licensing options
- * in this case.
- *
- * File: willpdone.php
- * Signature: 14b8e9d303477a3436372ee636523591
- * Date: Fri, 20 Apr 12 08:50:30 +0000
+ * MCCodes v2 by Dabomstew & ColdBlooded
+ * 
+ * Repository: https://github.com/davemacaulay/mccodesv2
+ * License: MIT License
  */
 
 global $h, $set;
